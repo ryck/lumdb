5efe967c6cb8857e01e9bde38c834c36582a8662
Ricardo Gonzalez <rickgc@gmail.com>
2018-04-06 11:21:42 +0100

    Second commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Movie from "./Movie";
+
+const API_KEY = "5ed7a59c30a22dd13ea34a42b5984ae0";
+
+class App extends Component {
+  state = {
+    movies: []
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
+      );
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results
+      });
+      console.log(movies);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    console.log(this.state.movies);
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Movie.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export default class Movie extends Component {
+  static propTypes = {
+    movie: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired
+    })
+  };
+
+  static defaultProps = {
+    desc: "Description not available"
+  };
+
+  render() {
+    const { title } = this.props.movie;
+    return (
+      <div>
+        <h3>{title}</h3>
+      </div>
+    );
+  }
+}
