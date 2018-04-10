f66f50a4ca5f634c369131378d386accb4698c5e
Ricardo Gonzalez <rickgc@gmail.com>
2018-04-10 16:03:18 +0100

    styled components

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import styled from "styled-components";
 import PropTypes from "prop-types";
 
 const POSTER_PATH = "http://image.tmdb.org/t/p/w154";
 
 const Movie = ({ movie }) => (
-  <div>
-    <Link to={`/${movie.id}`}>
-      <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
-    </Link>
-  </div>
+  <Link to={`/${movie.id}`}>
+    <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+  </Link>
 );
 
 export default Movie;
@@ -20,3 +18,7 @@ Movie.PropTypes = {
     title: PropTypes.string.isRequired
   }).isRequired
 };
+
+export const Poster = styled.img`
+  box-shadow: 0 0 35px black;
+`;
--- src/MoviesList.jsx
+++ src/MoviesList.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import Movie from "./Movie";
 
 class MoviesList extends Component {
@@ -23,12 +24,20 @@ class MoviesList extends Component {
   }
 
   render() {
+    const { movies } = this.state;
     return (
-      <div className="MoviesList">
-        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
-      </div>
+      <MovieGrid>
+        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+      </MovieGrid>
     );
   }
 }
 
 export default MoviesList;
+
+const MovieGrid = styled.div`
+  display: grid;
+  padding: 1rem;
+  grid-template-columns: repeat(6, 1fr);
+  grid-row-gap: 1rem;
+`;
